e6953063dc011a462d789901d5dfd8af26e45c1e
William <s3864077@rmit.edu.vn>
2022-09-04 17:02:47 +0700

    Initial Commit

---
+++ Ambrosia/AmbrosiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbrosiaApp.swift
+//  Ambrosia
+//
+//  Created by William on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AmbrosiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
