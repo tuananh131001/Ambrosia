3ce694b2997284b0df51b88c8509124fab35778e
Nguyen Tuan Anh <67695658+tuananh131001@users.noreply.github.com>
2022-09-11 12:06:00 +0700

    New API (#11)
    * 1. Image has display and restaurant name(cannot click )
    
    * 2. Refactor
    
    * Restaurant Detail working
    
    * 3.Image Detail working
    
    * new API
    
    * len

--- Ambrosia/Views/RestaurantViews/Container/RestaurantCard.swift
+++ Ambrosia/Views/RestaurantViews/Container/RestaurantCard.swift
@@ -14,26 +14,16 @@ import SwiftUI
 
 struct RestaurantCard: View {
     var rest: Restaurant
-    var cardWidth: CGFloat
-    var cardHeight: CGFloat
-    var displayType: String
+    var cardWidth: CGFloat = UIScreen.main.bounds.width - 30
+    var cardHeight: CGFloat = 357
+    var displayType: String = "all"
     
     var body: some View {
         // Card content
         // add display gone or not gone
         VStack(alignment: .center, spacing: 5) {
             // MARK: cover image
-            ZStack {
-                // Image of each map
-                AsyncImage(url: URL(string: "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=\(rest.photos?[0].photo_reference ?? "")&key=AIzaSyBtCts3HUN6SLrVPBY8LLsm4rNnleUtvZY")) { image in
-                    image.resizable()
-                    image.scaledToFit()
-                } placeholder: {
-                    Color.red
-                }
-                ImageShadowView()
-            }
-                .frame(width: cardWidth, height: cardHeight)
+            RestaurantAsyncImage(photo_id: rest.photos?[0].photo_reference ?? "").frame(width: cardWidth, height: cardHeight)
                 .cornerRadius(10)
                 .clipped()
             // MARK: Text
@@ -53,4 +43,5 @@ struct RestaurantCard: View {
             .frame(width: cardWidth)
     }
 }
+
 
