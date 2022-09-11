28a34da703a8b2ede659fc9d9baf27c3ccb18549
William <s3864077@rmit.edu.vn>
2022-09-11 12:49:35 +0700

    Update color

--- Ambrosia/Views/LaunchViews/LaunchContentView.swift
+++ Ambrosia/Views/LaunchViews/LaunchContentView.swift
@@ -22,7 +22,7 @@ struct LaunchContentView: View {
         ZStack {
             // for card
             Rectangle()
-                .foregroundColor(Color("DenyLocationBckColor"))
+                .foregroundColor(Color("PrimaryColor"))
 
             VStack (spacing: 30) {
 
@@ -65,46 +65,6 @@ struct LaunchContentView: View {
                 } label: {
                     Text("Sign Up Here!")
                 }
-                // MARK: Name of the app
-//                AppNameView()
-
-
-//                // MARK: open setting button
-//                Button {
-//                    // open app setting
-//                    if openSetting {
-//                        // Open settings by getting the settings url
-//                        if let url = URL(string: UIApplication.openSettingsURLString) {
-//                            if UIApplication.shared.canOpenURL(url) {
-//                                // If we can open this settings url, then open it
-//                                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-//                            }
-//                        }
-//                    }
-//                    // ask user permission window (when not been ask before)
-//                        else {
-//                        model.requestGeolocationPermission()
-//                    }
-//
-//                } label: {
-//                    // display button to use app
-//                    ZStack {
-//                        Rectangle()
-//                            .foregroundColor(.black)
-//                            .frame(height: 50)
-//                            .cornerRadius(15)
-//                            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.3), radius: 10, x: -5, y: 5)
-//
-//                        Text("Let's Eat")
-//                            .bold()
-//                            .foregroundColor(.white)
-//                            .padding()
-//                    }
-//
-//
-//                }
-
-
                 // MARK: gif
                 GifView(name: "cat-eat")
                     .frame(width: 173, height: 142)
