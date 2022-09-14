511d6367e5eee006a0d56880b55db4a1d22e2e0b
Matsuri <nhungmaitran1412@gmail.com>
2022-09-14 16:15:26 +0700

    merge commit sound model í đang bug đừng merge

--- Ambrosia/Views/RestaurantViews/FavouriteRestaurantView.swift
+++ Ambrosia/Views/RestaurantViews/FavouriteRestaurantView.swift
@@ -1,78 +1,126 @@
-
+// https://onmyway133.com/posts/how-to-use-foreach-with-indices-in-swiftui/
 
 import SwiftUI
 
 struct FavouriteRestaurantView: View {
     @EnvironmentObject var model: RestaurantModel
-    @EnvironmentObject var authModel: UserModel
-    
-    @State var rating: Int = 3
-    @State var isOpen: Bool = false
-    
+    @EnvironmentObject var userModel: UserModel
+    @StateObject var firebaseService: FirebaseService = FirebaseService.services
+
     @State var imageSize: CGFloat = 0.0
-    
+    @State var starSize: CGFloat = 0.0
+    @State var distanceSize: CGFloat = 0.0
+    @State var openSize: CGFloat = 0.0
+    @State var titleSize: CGFloat = 0.0
+
+    @State var clickFavourite: Bool = false
+//    @State var clickFavourites: [Bool]
+
+
+    init() {
+//        ratings = Array(repeating: 0, count: authModel.user.favouriteRestaurants.count)
+//        openStates = Array(repeating: false, count: authModel.user.favouriteRestaurants.count)
+//        clickFavourites = Array(repeating: false, count: userModel.user.favouriteRestaurants.count)
+    }
+
     var body: some View {
         GeometryReader { geo in
             NavigationView {
                 VStack {
                     ScrollView(.vertical) {
                         VStack {
-                            ForEach(authModel.user.favouriteRestaurants, id: \.place_id) { r in
+                            ForEach(userModel.user.favouriteRestaurants, id: \.place_id) { r in
                                 HStack {
                                     RestaurantAsyncImage(photo_id: r.photos?[0].photo_reference ?? "testRestaurant").frame(width: imageSize, height: imageSize).cornerRadius(10)
-                                    VStack(alignment: .leading, spacing: 0) {
+                                    VStack(alignment: .leading, spacing: 4) {
+                                        // MARK: rating
                                         HStack {
                                             Text("\(r.rating ?? 5.0, specifier: "%.1f")")
-                                                .foregroundColor(Color("Fv Special Clr"))
-                                            RatingView(rating: $rating, tappable: false, width: geo.size.width / 30, height: geo.size.width / 30)
+                                                .font(.system(size: starSize + 4))
+                                                .foregroundColor(Color("Fv Special Clr 2"))
+                                            ImageSystemHier(name: "star.fill", color: "Star On Color", size: starSize)
+
+                                            Button {
+                                                withAnimation(.easeOut(duration: 0.5)) {
+                                                    clickFavourite = firebaseService.changeFavorites(userModel: userModel, restaurant: r)
+//                                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//
+//                                                    }
+//                                                    clickFavourite = true
+                                                }
+                                            } label: {
+                                                ImageSystemHier(name: "heart\(clickFavourite ? ".fill" : "")", color: "Close Color", size: titleSize)
+                                                    .onAppear() {
+                                                    clickFavourite = userModel.isRestaurantFavorite(restaurant: r) == nil ? false : true
+                                                }
+                                            }
+
                                         }
+
+                                        // MARK: name
                                         Text(r.name)
                                             .lineLimit(1)
                                             .foregroundColor(Color("Fv Title Clr"))
-                                        
-                                        HStack {
-                                            Image(systemName: "car.fill")
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: geo.size.width / 35)
-                                                .background(Color("Fv Subtitle Clr 2"))
-                                            Text("\(r.distance, specifier: "%.2f")km")
-                                                .foregroundColor(Color("Fv Subtitle Clr"))
+                                            .font(.system(size: titleSize))
+
+                                        // MARK: side info
+                                        HStack(spacing: 5) {
+                                            // MARK: Restaurant type
                                             Text("\(model.type ?? "Inexpensive")")
-                                                .foregroundColor(Color("Fv Subtitle Clr 2"))
+                                                .foregroundColor(Color("Fv Subtitle Clr"))
+                                                .padding(.trailing, 3)
+                                                .onAppear() {
+                                                    model.getType(r.price_level)
+                                                }
+
+                                            Text("✼")
+                                                .font(.system(size: distanceSize + 1))
+                                                .foregroundColor(Color("Fv Special Clr"))
+                                                .bold()
+
+                                            // MARK: Restaurant distance
+                                            HStack {
+                                                ImageSystemHier(name: "car.fill", color: "Fv Subtitle Clr 2", size: distanceSize + 1)
+                                                Text("\(r.distance, specifier: "%.2f")km")
+                                                    .foregroundColor(Color("Fv Subtitle Clr 2"))
+                                            }
                                         }
-                                        
+                                            .font(.system(size: distanceSize))
+                                            .padding(.top, 3)
+
                                         Spacer()
-                                        
-                                        Text(isOpen ? "OPEN" : "CLOSED")
-                                            .foregroundColor(Color("\(isOpen ? "Open" : "Close") Color"))
+
+                                        // MARK: Open State
+                                        HStack(spacing: 3) {
+                                            let isOpen = r.opening_hours?.open_now ?? true
+                                            ImageSystemHier(name: "clock.badge.checkmark.fill", color: "Fv Subtitle Clr 2", size: openSize + 1)
+                                            Text(isOpen ? "OPEN" : "CLOSED")
+                                                .foregroundColor(Color("\(isOpen ? "Open" : "Close") Color"))
+                                                .font(.system(size: openSize))
+                                        }
                                     }
                                     Spacer()
                                 }
-                                .onAppear() {
-                                    rating = Int(r.rating ?? 0)
+                                .opacity(clickFavourite ? 1 : 0.5)
+                                    .onAppear() {
                                     model.getType(r.price_level)
-                                    isOpen = r.opening_hours?.open_now ?? true
-                                    
                                     imageSize = geo.size.width / 3
+                                    starSize = geo.size.width / 30
+                                    distanceSize = geo.size.width / 27
+                                    openSize = geo.size.width / 25
+                                    titleSize = geo.size.width / 21
+                                        
+                                        
                                 }
-                                
+
                             }
                         }
                     }
                 }
-                .navigationTitle("Favourite")
-                
+                    .navigationTitle("Favourite")
+
             }
         }
 
-    }
-}
-
-struct FavouriteRestaurantView_Previews: PreviewProvider {
-    static var previews: some View {
-        FavouriteRestaurantView()
-            .environmentObject(RestaurantModel())
-            .environmentObject(AuthenticationModel())
     }
 }
