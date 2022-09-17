37af503868ac17a2bfb8e6eb7dda37892cfaee0e
Huy ne <bmhuyquoc104@gmail.com>
2022-09-18 01:48:23 +0700

    Add search for favorite restaurant

--- Ambrosia/Views/FavoriteView/FavouriteRestaurantView.swift
+++ Ambrosia/Views/FavoriteView/FavouriteRestaurantView.swift
@@ -1,17 +1,17 @@
 /*
-    RMIT University Vietnam
-    Course: COSC2659 iOS Development
-    Semester: 2022B
-    Assessment: Assignment 3
-    Author: Tran Mai Nhung
-    ID: s3879954
-    Created  date: 15/09/2022
-    Last modified: 17/09/2022
-    Acknowledgement:
-    - Canvas
-    - https://onmyway133.com/posts/how-to-use-foreach-with-indices-in-swiftui/
-    - https://swiftbysundell.com/articles/stroking-and-filling-a-swiftui-shape-at-the-same-time
-*/
+ RMIT University Vietnam
+ Course: COSC2659 iOS Development
+ Semester: 2022B
+ Assessment: Assignment 3
+ Author: Tran Mai Nhung
+ ID: s3879954
+ Created  date: 15/09/2022
+ Last modified: 17/09/2022
+ Acknowledgement:
+ - Canvas
+ - https://onmyway133.com/posts/how-to-use-foreach-with-indices-in-swiftui/
+ - https://swiftbysundell.com/articles/stroking-and-filling-a-swiftui-shape-at-the-same-time
+ */
 
 
 import SwiftUI
@@ -20,7 +20,7 @@ import SwiftUI
 struct FavouriteRestaurantView: View {
     @EnvironmentObject var restaurantModel: RestaurantModel
     @EnvironmentObject var userModel: UserModel
-
+    
     var cardWidth: CGFloat = 0.0
     var imageSize: CGFloat = 120
     var cardHeight: CGFloat = 135.0
@@ -29,6 +29,19 @@ struct FavouriteRestaurantView: View {
             .font(.title)
             .bold()
             .foregroundColor(Constants.PRIMARY_COLOR)
+    }
+    @State private var searchText = ""
+    var searchResults: [Restaurant] {
+        // if the search bar is empty -> show all
+        if searchText.isEmpty {
+            return userModel.user.favouriteRestaurants
+            
+            
+        } else {
+            return userModel.user.favouriteRestaurants.filter{ $0.title.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+        
     }
     var body: some View {
         GeometryReader { geo in
@@ -40,12 +53,12 @@ struct FavouriteRestaurantView: View {
                         VStack(alignment: .leading, spacing: 50) {
                             ScrollView {
                                 VStack(spacing: 30) {
-                                    ForEach(userModel.user.favouriteRestaurants, id: \.placeId) { rest in
+                                    ForEach(searchResults, id: \.placeId) { rest in
                                         FavoriteCard(imageSize: imageSize, cardHeight: cardHeight, geo: geo, rest: rest)
                                     }
                                 }
                             }
-                        }
+                        }.searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search by restaurant's name")
                     }
                     else {
                         FavoriteNotFound(geo: geo)
@@ -55,5 +68,5 @@ struct FavouriteRestaurantView: View {
             }
         }
     }
-
+    
 }
